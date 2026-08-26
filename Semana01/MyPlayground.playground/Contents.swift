f29d5a8a01793f83469367471b4830b33f2ab38f
Kevin Barbaran <kevin.barbaran@tecsup.edu.pe>
2026-08-26 09:43:28 -0500

    Segundo commit hecho en clase, complementación.

--- Semana01/MyPlayground.playground/Contents.swift
+++ Semana01/MyPlayground.playground/Contents.swift
@@ -6,7 +6,7 @@ import Foundation
 let igv = 0.18
 
 func    CalcularDesc(cantidad: Int, subtotal: Double)->Double{
-    if cantidad >  10 {
+    if cantidad >   10 {
         return subtotal * 0.15
     } else if cantidad > 5 {
         return subtotal * 0.10
@@ -16,19 +16,32 @@ func    CalcularDesc(cantidad: Int, subtotal: Double)->Double{
 }
 
 print("ingrese el nombre del producto")
-let producto = readLine() ?? ""
+let producto = readLine() ?? "Leche"
 print("ingrese el precio del producto")
-let precio = Double(readLine() ?? "0") ?? 0.0
+let precio = Double(readLine() ?? "12.0") ?? 12.0
 print("ingrese la cantidad del producto")
-let cantidad = Int(readLine() ?? "0") ?? 0
+let cantidad = Int(readLine() ?? "5") ?? 5
 
 let subtotal = precio * Double(cantidad)
 let descuento = CalcularDesc(cantidad: cantidad, subtotal: subtotal)
 
 //halalr el monto base : subtotal - descuento
+let montoBase = subtotal - descuento
+
 //hallar el igv : monto base * igv
+let totalIGV = montoBase * igv
+
 //hallar el total : monto base + igv
+let totalPagar = montoBase + totalIGV
 
 //primer commit
 
 //mostrar los datos.
+print("Producto: \(producto)")
+print("Precio unitario: S/ \(precio)")
+print("Cantidad: \(cantidad)")
+print("Subtotal: S/ \(subtotal)")
+print("Descuento: S/ \(descuento)")
+print("Monto base: S/ \(montoBase)")
+print("IGV (18%): S/ \(totalIGV)")
+print("Total a pagar: S/ \(totalPagar)")
