d1b8ed512d07f9fabd99dd4a51d3539c46e34cb0
Kevin Barbaran <kevin.barbaran@tecsup.edu.pe>
2026-08-26 10:12:22 -0500

    Segundo commit del ejercicio 2/4

--- Semana02/Semana02Ejercicio01.playground/Contents.swift
+++ Semana02/Semana02Ejercicio01.playground/Contents.swift
@@ -2,7 +2,7 @@ import Foundation
 
 //Calendario de pagos
 
-//El interés según las cuotas (6 -> 20%, 12 -> 40%, 24 -> 60%)
+//El interés según las cuotas (6 : 20%, 12 : 40%, 24 : 60%)
 func calcularInteres(meses: Int, montoCompra: Double) -> Double {
     if meses == 6 {
         return montoCompra * 0.20
@@ -14,3 +14,18 @@ func calcularInteres(meses: Int, montoCompra: Double) -> Double {
         return 0.0
     }
 }
+
+//lectura de datos
+print("Nombre del Producto:")
+let producto = readLine() ?? ""
+print("Precio:")
+let precioUnit = Double(readLine() ?? "0") ?? 0.0
+print("Cantidad:")
+let cantidad = Int(readLine() ?? "0") ?? 0
+
+//monto total de la compra
+let montoCompra = precioUnit * Double(cantidad)
+print("monto total de la compra : \(montoCompra)")
+
+print("Elige el plan de pago (6, 12, 24):")
+let meses = Int(readLine() ?? "0") ?? 6
