0df596d4f4520ebfbe326885c60e80ebfba4c844
Kevin Barbaran <kevin.barbaran@tecsup.edu.pe>
2026-08-26 09:55:17 -0500

    Primer commit avance

---
+++ Semana01/Ejercicio2Sem01.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+//Calendario de pagos/
+
+//El interés según las cuotas (6 : 20%, 12 : 40%, 24 : 60%)
+func calcularInteres(meses: Int, montoCompra: Double) -> Double {
+    if meses == 6 {
+        return montoCompra * 0.20
+    } else if meses == 12 {
+        return montoCompra * 0.40
+    } else if meses == 24 {
+        return montoCompra * 0.60
+    } else {
+        return 0.0
+    }
+}
+
