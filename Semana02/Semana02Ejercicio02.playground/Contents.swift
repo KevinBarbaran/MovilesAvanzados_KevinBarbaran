7b5088b7f3c1498b16722403ae35a00340149628
Kevin Barbaran <Kevin.barbaran@tecsup.edu.pe>
2026-09-02 08:40:31 -0500

    Segundo commit - avance.

--- Semana02/Semana02Ejercicio02.playground/Contents.swift
+++ Semana02/Semana02Ejercicio02.playground/Contents.swift
@@ -10,3 +10,31 @@ func calcularInteres(meses: Int, montoCompra: Double) -> Double {
     }
 }
 
+// 2. Ingreso de datos
+print("Nombre Producto:")
+let productoRead = readLine() ?? ""
+let producto = productoRead.isEmpty ? "Laptop" : productoRead
+
+print("Precio Unitario:")
+let precioRead = Double(readLine() ?? "0") ?? 0.0
+let precioUnit = precioRead == 0 ? 2500.0 : precioRead
+
+print("Cantidad:")
+let cantidadRead = Int(readLine() ?? "0") ?? 0
+let cantidad = cantidadRead == 0 ? 1 : cantidadRead
+
+let montoCompra = precioUnit * Double(cantidad)
+print("MONTO TOTAL COMPRA: S/ \(montoCompra)")
+
+// Validar el Plan de Pago (6, 12, 24)
+print("Plan de Pago (6, 12, 24):")
+let mesesIngresados = Int(readLine() ?? "0") ?? 12
+let meses = [6, 12, 24].contains(mesesIngresados) ? mesesIngresados : 12
+
+// Preguntas sobre el Pago Adelantado
+print("¿En qué mes hará un Pago Adelantado? (0 = Ninguno):")
+let mesAdelantado = Int(readLine() ?? "0") ?? 3
+
+print("Monto Adicional a Pagar ese Mes (S/.):")
+let montoAdicional = Double(readLine() ?? "0") ?? 1000.0
+
