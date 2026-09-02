2e7d88c675ed1d3d921b5b15626f5f89dbf15529
Kevin Barbaran <Kevin.barbaran@tecsup.edu.pe>
2026-09-02 11:20:05 -0500

    calcular impuestos de IGV y clasificar categoría del cliente

--- semana03/Semana03Ejercicio05Carrito.playground/Contents.swift
+++ semana03/Semana03Ejercicio05Carrito.playground/Contents.swift
@@ -63,3 +63,16 @@ if totalCarrito >= 5000 {
 
 let descuento = totalCarrito * descPct
 let totalConDesc = totalCarrito - descuento
+
+// TODO 16: IGV y total
+let igv = totalConDesc * 0.18
+let totalFinal = totalConDesc + igv
+
+// TODO 17: Categoría
+var categoria = ""
+switch Int(totalCarrito) {
+case 0..<500: categoria = "Regular"
+case 500..<2000: categoria = "Frecuente"
+case 2000..<5000: categoria = "VIP"
+default: categoria = "Premium"
+}
